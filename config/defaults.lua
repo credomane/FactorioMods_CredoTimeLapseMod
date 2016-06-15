063a108d402af6d4d5c66c492dd1535a487e1d29
Credomane <github@credomane.com>
2016-06-15 00:39:07 -0500

    Revised config defaults.

--- config/defaults.lua
+++ config/defaults.lua
@@ -5,15 +5,16 @@
     I can't stop you though so do what you want. :D
 ]]
 
-local config_defaults = {};
+config_defaults = {};
+config_defaults.version = MOD_VERSION;
+
 config_defaults.players = {};
 config_defaults.positions = {};
 config_defaults.surfaces = {};
 
-config_defaults.enabled = false; -- Mod requires some basic config in gui first. 
-config_defaults.gameSpeedTooFast = false; -- game.speed > 3 ?
+config_defaults.enabled = false; -- Mod requires some basic config in gui first.
 
-config_defaults.saveFolder = ""; -- saves to script-output/CTLM/<saveFolder>/
+config_defaults.saveFolder = "temp"; -- saves to script-output/CTLM/<saveFolder>/
 config_defaults.screenshotNumber = 0; -- Last number used for a screenshot to prevent overwriting older screenshots.
 config_defaults.screenshotInterval = 18000; --[[Approximately 5 minutes worth of game ticks.
     WARNING: mod will NOT take screenshots when game.speed is greater than 3!
@@ -23,34 +24,34 @@ config_defaults.screenshotInterval = 18000; --[[Approximately 5 minutes worth of
     z=60; which is 60 ticks in a second.
 ]]
 
-config_defaults.player_defaults = {};
-config_defaults.player_defaults.enabled = false;
-config_defaults.player_defaults.zoom = 1; -- zoom > 0; Zoom level is allowed to be a decimal. i.e. 0.12345
-config_defaults.player_defaults.width = 1920;
-config_defaults.player_defaults.height = 1080;
-config_defaults.player_defaults.show_gui = false;
-config_defaults.player_defaults.show_altinfo = false;
-config_defaults.player_defaults.dayOnly = true;
+config_player_defaults = {};
+config_player_defaults.enabled = false;
+config_player_defaults.zoom = 1; -- zoom > 0; Zoom level is allowed to be a decimal. i.e. 0.12345
+config_player_defaults.width = 1920;
+config_player_defaults.height = 1080;
+config_player_defaults.show_gui = false;
+config_player_defaults.show_altinfo = false;
+config_player_defaults.dayOnly = true;
 
 
-config_defaults.position_defaults = {};
-config_defaults.position_defaults.enabled = true;
-config_defaults.position_defaults.zoom = 0.65; -- zoom > 0; Zoom level is allowed to be a decimal. i.e. 0.12345
-config_defaults.position_defaults.width = 1920;
-config_defaults.position_defaults.height = 1080;
-config_defaults.position_defaults.positionX = 0;
-config_defaults.position_defaults.positionY = 0;
-config_defaults.position_defaults.show_gui = false;
-config_defaults.position_defaults.show_altinfo = true;
-config_defaults.position_defaults.dayOnly = true;
+config_position_defaults = {};
+config_position_defaults.enabled = true;
+config_position_defaults.zoom = 0.65; -- zoom > 0; Zoom level is allowed to be a decimal. i.e. 0.12345
+config_position_defaults.width = 1920;
+config_position_defaults.height = 1080;
+config_position_defaults.positionX = 0;
+config_position_defaults.positionY = 0;
+config_position_defaults.show_gui = false;
+config_position_defaults.show_altinfo = true;
+config_position_defaults.dayOnly = true;
 
-config_defaults.surface_defaults = {};
-config_defaults.surface_defaults.enabled = true;
-config_defaults.surface_defaults.zoom = 0.65; -- zoom > 0; Zoom level is allowed to be a decimal. i.e. 0.12345
-config_defaults.surface_defaults.width = 1920;
-config_defaults.surface_defaults.height = 1080;
-config_defaults.surface_defaults.positionX = 0;
-config_defaults.surface_defaults.positionY = 0;
-config_defaults.surface_defaults.show_gui = false;
-config_defaults.surface_defaults.show_altinfo = true;
-config_defaults.surface_defaults.dayOnly = true;
+config_surface_defaults = {};
+config_surface_defaults.enabled = false;
+config_surface_defaults.zoom = 0.65; -- zoom > 0; Zoom level is allowed to be a decimal. i.e. 0.12345
+config_surface_defaults.width = 1920;
+config_surface_defaults.height = 1080;
+config_surface_defaults.positionX = 0;
+config_surface_defaults.positionY = 0;
+config_surface_defaults.show_gui = false;
+config_surface_defaults.show_altinfo = true;
+config_surface_defaults.dayOnly = true;
