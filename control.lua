2831763ee961406bf2a1fa66e39ab194663bdb21
Credomane <github@credomane.com>
2016-06-15 18:39:44 -0500

    Seems Factorio runs every mods as an isolated instances. Removed CTLM object as it was PITA and pointless.
    Moved config loading to central function

--- control.lua
+++ control.lua
@@ -9,54 +9,49 @@ require "config.defaults"
 
 MOD_NAME = "CTLM";
 MOD_VERSION = "0.0.1";
-CONFIG_NAME = MOD_NAME .. "_config";
 
-CTLM = {};
 config = {};
 
-function CTLM:init()
-CTLM.debug("BEG on_init");
-    if not global[CONFIG_NAME] then
-        global[CONFIG_NAME] = CTLM.deepCopy(config_defaults);
-    end
-
-    CTLM.load();
-CTLM.debug("END on_init");
+function init()
+debuglog("BEG on_init");
+    initConfig()
+    load();
+debuglog("END on_init");
 end
 
-function CTLM:load()
-CTLM.debug("BEG on_load");
-    config = global[CONFIG_NAME];
-CTLM.debug("END on_load");
+function load()
+debuglog("BEG on_load");
+    initConfig()
+debuglog("END on_load");
 end
 
-function CTLM:tick()
+function tick()
     if game.speed > 3 or not config.enabled then
         return;
     end
 
     if game.tick % (game.speed * config.screenshotInterval ) == 0 then
         --Time to loop through them screenshots!
-        CTLM.debug("Taking screenshots...");
+        debuglog("Taking screenshots...");
         local screenshotTaken = false;
         for index, player in ipairs(game.players) do
             if player.valid and player.connected and config.players[index] and config.players[index].enabled then
                 screenshotTaken = true;
-                CTLM.playerScreenshot(index);
+                playerScreenshot(index);
             end
         end
 
         for index, position in ipairs(config.positions) do
             if config.positions[index] and config.positions[index].enabled then
                 screenshotTaken = true;
-                CTLM.positionScreenshot(index);
+                positionScreenshot(index);
             end
         end
 
         for index, surface in pairs(game.surfaces) do
             if surface.valid and config.surfaces[index] and config.surfaces[index].enabled then
                 screenshotTaken = true;
-                CTLM.surfaceScreenshot(index);
+                surfaceScreenshot(index);
             end
         end
 
@@ -70,8 +65,16 @@ end
 --Helper functions are the bees knees!
 --------------------------------------
 
---CTLM:*screenshot functions!
-function CTLM:playerScreenshot(index)
+--Fix/install config!
+function initConfig()
+    if not global.config then
+        global.config = deepCopy(config_defaults);
+    end
+    config = global.config;
+end
+
+--screenshot functions!
+function playerScreenshot(index)
     local currentTime = game.daytime;
     if config.players[index].dayOnly then
         game.daytime = 0;
@@ -81,16 +84,16 @@ function CTLM:playerScreenshot(index)
         player = game.players[index],
         resolution = { config.players[index].width, config.players[index].height },
         zoom = config.players[index].zoom,
-        path = CTLM.genFilename("player", game.players[index].name),
+        path = genFilename("player", game.players[index].name),
         config.players[index].show_gui,
         config.players[index].show_altinfo
     });
 
     game.daytime = currentTime;
-    CTLM.printall("playerScreenshot: " .. game.players[index].name);
+    printall("playerScreenshot: " .. game.players[index].name);
 end
 
-function CTLM:positionScreenshot(index)
+function positionScreenshot(index)
     local currentTime = game.daytime;
     if config.positions[index].dayOnly then
         game.daytime = 0;
@@ -101,16 +104,16 @@ function CTLM:positionScreenshot(index)
         position = { config.positions[index].positionX, config.positions[index].positionY },
         resolution = { config.positions[index].width, config.positions[index].height },
         zoom = config.positions[index].zoom,
-        path = CTLM.genFilename("position", game.positions[index].name),
+        path = genFilename("position", game.positions[index].name),
         config.positions[index].show_gui,
         config.positions[index].show_altinfo
     });
 
     game.daytime = currentTime;
-    CTLM.printall("positionScreenshot: " .. game.positions[index].name);
+    printall("positionScreenshot: " .. game.positions[index].name);
 end
 
-function CTLM:surfaceScreenshot(index)
+function surfaceScreenshot(index)
     local currentTime = game.daytime;
     if config.surfaces[index].dayOnly then
         game.daytime = 0;
@@ -121,41 +124,41 @@ function CTLM:surfaceScreenshot(index)
         position = { config.surfaces[index].positionX, config.surfaces[index].positionY },
         resolution = { config.surfaces[index].width, config.surfaces[index].height },
         zoom = config.surfaces[index].zoom,
-        path = CTLM.genFilename("surface", game.surfaces[index].name),
+        path = genFilename("surface", game.surfaces[index].name),
         config.surfaces[index].show_gui,
         config.surfaces[index].show_altinfo
     });
 
     game.daytime = currentTime;
-    CTLM.printall("surfaceScreenshot: " .. game.surfaces[index].name);
+    printall("surfaceScreenshot: " .. game.surfaces[index].name);
 end
 
---CTLM:add*(index) functions
-function CTLM:addPlayer(index)
+--add*(index) functions
+function addPlayer(index)
     if not config.players[index] then
-        config.players[index] = CTLM.deepCopy(config_player_defaults);
+        config.players[index] = deepCopy(config_player_defaults);
     end
 end
 
-function CTLM:addPosition(index)
+function addPosition(index)
     if not config.positions[index] then
-        config.positions[index] = CTLM.deepCopy(config_position_defaults);
+        config.positions[index] = deepCopy(config_position_defaults);
     end
 end
 
-function CTLM:addSurface(index)
+function addSurface(index)
     if not config.surfaces[index] then
-        config.surfaces[index] = CTLM.deepCopy(config_surface_defaults);
+        config.surfaces[index] = deepCopy(config_surface_defaults);
     end
 end
 
 --File name to save screenshot as
-function CTLM:GenFilename(screenshotType, screenshotName)
+function GenFilename(screenshotType, screenshotName)
     return MOD_NAME .. "/" .. screenshotType .. "/" .. screenshotName .. "/" .. string.format("%05d", config.screenshotNumber) .. ".png";
 end
 
 --Print a message to ALL players currently connected. Only used in debugging but left in release so it isn't lost.
-function CTLM:printall(msg)
+function printall(msg)
     for index, player in ipairs(game.players) do
         if player.valid and player.connected then
             player.print(msg);
@@ -163,7 +166,7 @@ function CTLM:printall(msg)
     end
 end
 
-function CTLM:debug(msg)
+function debug(msg)
     if not game then
         return;
     end
@@ -173,7 +176,7 @@ function CTLM:debug(msg)
 end
 
 --Special function to "deep" copy a table to avoid references. That would be bad. :/
-function CTLM:deepCopy(object)
+function deepCopy(object)
     local lookup_table = {}
     local function _copy(object)
         if type(object) ~= "table" then
@@ -192,7 +195,7 @@ function CTLM:deepCopy(object)
 end
 
 --Register event handlers!
-script.on_init(CTLM.init);
-script.on_load(CTLM.load);
-script.on_event(defines.events.on_tick, CTLM.tick);
+script.on_init(init);
+script.on_load(load);
+script.on_event(defines.events.on_tick, tick);
 
