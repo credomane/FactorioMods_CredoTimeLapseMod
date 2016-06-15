9e43dc6fc420b3c1a2f2f65962aa33710f62d17f
Credomane <github@credomane.com>
2016-06-15 00:47:53 -0500

    Fixed parse errors.
    Fixed issues with config save/load
    
    More robust table copy

--- control.lua
+++ control.lua
@@ -7,69 +7,56 @@ require "defines"
 require "config.defaults"
 
 
-local MOD_NAME = "CTLM";
-local MOD_VERSION = "0.0.1";
-local CONFIG_NAME = MOD_NAME .. "_config";
-local CTLM = {};
-local config = {};
+MOD_NAME = "CTLM";
+MOD_VERSION = "0.0.1";
+CONFIG_NAME = MOD_NAME .. "_config";
+
+CTLM = {};
+config = {};
 
 function CTLM:init()
-    self.printall("Init");
-    for index, player in ipairs(game.players) do
-        if player.valid and player.connected then
-            self.printall("Init.player: " + player.name);
-        end
+CTLM.debug("BEG on_init");
+    if not global[CONFIG_NAME] then
+        global[CONFIG_NAME] = CTLM.deepCopy(config_defaults);
     end
+
+    CTLM.load();
+CTLM.debug("END on_init");
 end
 
 function CTLM:load()
-    _G[ CONFIG_NAME ] = _G[ CONFIG_NAME ] or config_defaults;
-    config = _G[ CONFIG_NAME ];
-
-    if not config.version then
-        config.version = MOD_VERSION;
-    end
-
-    self.printall("Load");
-    for index, player in ipairs(game.players) do
-        if player.valid and player.connected then
-            self.printall("Load.player: " + player.name);
-        end
-    end
+CTLM.debug("BEG on_load");
+    config = global[CONFIG_NAME];
+CTLM.debug("END on_load");
 end
 
 function CTLM:tick()
-    if game.speed > 3 then
-        config.gameSpeedTooFast=true;
-    else
-        config.gameSpeedTooFast=false;
-    end
-
-    if config.gameSpeedTooFast or not config.enabled then
+    if game.speed > 3 or not config.enabled then
         return;
     end
 
     if game.tick % (game.speed * config.screenshotInterval ) == 0 then
         --Time to loop through them screenshots!
+        CTLM.debug("Taking screenshots...");
         local screenshotTaken = false;
         for index, player in ipairs(game.players) do
             if player.valid and player.connected and config.players[index] and config.players[index].enabled then
                 screenshotTaken = true;
-                self.playerScreenshot(index);
+                CTLM.playerScreenshot(index);
             end
         end
 
         for index, position in ipairs(config.positions) do
             if config.positions[index] and config.positions[index].enabled then
                 screenshotTaken = true;
-                self.positionScreenshot(index);
+                CTLM.positionScreenshot(index);
             end
         end
 
-        for index, surface in ipairs(game.surfaces) do
+        for index, surface in pairs(game.surfaces) do
             if surface.valid and config.surfaces[index] and config.surfaces[index].enabled then
                 screenshotTaken = true;
-                self.surfaceScreenshot(index);
+                CTLM.surfaceScreenshot(index);
             end
         end
 
@@ -94,13 +81,13 @@ function CTLM:playerScreenshot(index)
         player = game.players[index],
         resolution = { config.players[index].width, config.players[index].height },
         zoom = config.players[index].zoom,
-        path = self.genFilename("player", game.players[index].name),
+        path = CTLM.genFilename("player", game.players[index].name),
         config.players[index].show_gui,
         config.players[index].show_altinfo
     });
 
     game.daytime = currentTime;
-    self.printall("playerScreenshot: " + game.players[index].name);
+    CTLM.printall("playerScreenshot: " .. game.players[index].name);
 end
 
 function CTLM:positionScreenshot(index)
@@ -114,13 +101,13 @@ function CTLM:positionScreenshot(index)
         position = { config.positions[index].positionX, config.positions[index].positionY },
         resolution = { config.positions[index].width, config.positions[index].height },
         zoom = config.positions[index].zoom,
-        path = self.genFilename("position", game.positions[index].name),
+        path = CTLM.genFilename("position", game.positions[index].name),
         config.positions[index].show_gui,
         config.positions[index].show_altinfo
     });
 
     game.daytime = currentTime;
-    self.printall("positionScreenshot: " + game.positions[index].name);
+    CTLM.printall("positionScreenshot: " .. game.positions[index].name);
 end
 
 function CTLM:surfaceScreenshot(index)
@@ -130,35 +117,35 @@ function CTLM:surfaceScreenshot(index)
     end
 
     game.take_screenshot({
-        surface = game.surfaces[index]
+        surface = game.surfaces[index],
         position = { config.surfaces[index].positionX, config.surfaces[index].positionY },
         resolution = { config.surfaces[index].width, config.surfaces[index].height },
         zoom = config.surfaces[index].zoom,
-        path = self.genFilename("player", game.surfaces[index].name),
+        path = CTLM.genFilename("surface", game.surfaces[index].name),
         config.surfaces[index].show_gui,
         config.surfaces[index].show_altinfo
     });
 
     game.daytime = currentTime;
-    self.printall("surfaceScreenshot: " + game.surfaces[index].name);
+    CTLM.printall("surfaceScreenshot: " .. game.surfaces[index].name);
 end
 
 --CTLM:add*(index) functions
 function CTLM:addPlayer(index)
     if not config.players[index] then
-        config.players[index] = self.tableCopy(config_defaults.player_defaults);
+        config.players[index] = CTLM.deepCopy(config_player_defaults);
     end
 end
 
 function CTLM:addPosition(index)
     if not config.positions[index] then
-        config.positions[index] = self.tableCopy(config_defaults.position_defaults);
+        config.positions[index] = CTLM.deepCopy(config_position_defaults);
     end
 end
 
 function CTLM:addSurface(index)
     if not config.surfaces[index] then
-        config.surfaces[index] = self.tableCopy(config_defaults.surface_defaults);
+        config.surfaces[index] = CTLM.deepCopy(config_surface_defaults);
     end
 end
 
@@ -176,12 +163,32 @@ function CTLM:printall(msg)
     end
 end
 
+function CTLM:debug(msg)
+    if not game then
+        return;
+    end
+--    if config.debug then
+        game.write_file(MOD_NAME .. "/debug.log", msg .. "\n", true);
+--    end
+end
+
 --Special function to "deep" copy a table to about references. That would be bad. :/
-function CTLM:tableCopy(obj)
-  if type(obj) ~= 'table' then return obj end
-  local res = {}
-  for k, v in pairs(obj) do res[self.TableCopy(k)] = self.TableCopy(v) end
-  return res
+function CTLM:deepCopy(object)
+    local lookup_table = {}
+    local function _copy(object)
+        if type(object) ~= "table" then
+            return object
+        elseif lookup_table[object] then
+            return lookup_table[object]
+        end
+        local new_table = {}
+        lookup_table[object] = new_table
+        for index, value in pairs(object) do
+            new_table[_copy(index)] = _copy(value)
+        end
+        return setmetatable(new_table, getmetatable(object))
+    end
+    return _copy(object)
 end
 
 --Register event handlers!
