1dac186c16eff745a72160c9dd25cf74170d4457
Credomane <github@credomane.com>
2016-06-19 01:51:35 -0500

    Added player edit UI

--- CTLM/gui/players.lua
+++ CTLM/gui/players.lua
@@ -44,7 +44,7 @@ function CTLM.gui.CTLM_settings_players_open(event)
         caption={"settings.players.title"}
     });
 
-    --players frame buttons
+    --root frame buttons
     local buttons = rootFrame.add({type="flow", name="buttons", direction="horizontal"});
     buttons.style.minimal_width = 500;
     buttons.style.maximal_width = 1000;
@@ -59,7 +59,22 @@ function CTLM.gui.CTLM_settings_players_open(event)
         style="naked_frame_style"
     });
 
+    local subFrameCounter = 0;
+    local function newSubFrame()
+        CTLM.log("newSubFrame(" .. subFrameCounter .. ")");
+        local frame = mainFrame.add({
+            type="frame",
+            name="sub" .. subFrameCounter,
+            direction="horizontal",
+            style="naked_frame_style"
+        });
+        subFrameCounter = subFrameCounter + 1;
+        return frame;
+    end
+
     local playerName = "";
+    local subFrame = newSubFrame();
+    local buttonCounter = 0;
     for index, player in pairs(game.players) do
         if not player.name or player.name == "" then
             playerName = "UnnamedPlayer" .. player.index;
@@ -70,7 +85,12 @@ function CTLM.gui.CTLM_settings_players_open(event)
             global.players[playerName] = CTLM.deepCopy(config.player_defaults);
         end
 
-        mainFrame.add({type="button", name="CTLM_settings_player_button_" .. playerName, caption=playerName});
+        subFrame.add({type="button", name="CTLM_settings_playerEdit_open_" .. playerName, caption=playerName});
+
+        buttonCounter = buttonCounter + 1;
+        if buttonCounter % 5 == 0 then
+            subFrame = newSubFrame();
+        end
     end
 end
 
@@ -83,4 +103,127 @@ end
 
 function CTLM.gui.CTLM_settings_players_save(event)
     local player = game.get_player(event.player_index);
+end
+
+------------------------------------------------------------------------------------------------------
+--PLAYER EDIT SETTINGS
+------------------------------------------------------------------------------------------------------
+function CTLM.gui.CTLM_settings_playerEdit_open(event)
+    local player = game.get_player(event.player_index);
+    local playerEditName = string.trimStart(event.element.name, "CTLM_settings_playerEdit_open_");
+
+    if player.gui.center.CTLM_settings_playerEdit ~= nil then
+        player.gui.center.CTLM_settings_playerEdit.destroy();
+    end
+
+    CTLM.log("playerEditName:" .. playerEditName);
+
+    --root frame
+    local rootFrame = player.gui.center.add({
+        type="frame",
+        direction="vertical",
+        name="CTLM_settings_playerEdit",
+        caption={"settings.playerEdit.title"}
+    });
+
+    --main frame
+    local mainFrame = rootFrame.add({
+        type="frame",
+        name="main",
+        direction="vertical",
+        caption=playerEditName,
+        style="naked_frame_style"
+    });
+
+    --[beg] Main frame -> player enabled setting
+    local enabled_flow = mainFrame.add({type="flow", name="enabled_flow", direction="horizontal"});
+    enabled_flow.add({type="checkbox", name="checkbox", caption={"settings.playerEdit.enabled"}, state=global.players[playerEditName].enabled});
+    --[end] Main frame -> player enabled setting
+
+    --[beg] Main frame -> dayOnly setting
+    local dayOnly_flow = mainFrame.add({type="flow", name="dayOnly_flow", direction="horizontal"});
+    dayOnly_flow.add({type="checkbox", name="checkbox", caption={"settings.playerEdit.dayOnly"}, state=global.players[playerEditName].dayOnly});
+    --[end] Main frame -> dayOnly setting
+
+    --[beg] Main frame -> width setting
+    local width_flow = mainFrame.add({type="flow", name="width_flow", direction="horizontal"});
+    width_flow.add({type="label", caption={"settings.playerEdit.width"}});
+    local textfield = width_flow.add({type="textfield", name="textfield", style="number_textfield_style"});
+    textfield.text=global.players[playerEditName].width;
+    textfield.style.minimal_width = 50;
+    textfield.style.maximal_width = 50;
+    --[end] Main frame -> width setting
+
+    --[beg] Main frame -> height setting
+    local height_flow = mainFrame.add({type="flow", name="height_flow", direction="horizontal"});
+    height_flow.add({type="label", caption={"settings.playerEdit.height"}});
+    local textfield = height_flow.add({type="textfield", name="textfield", style="number_textfield_style"});
+    textfield.text=global.players[playerEditName].height;
+    textfield.style.minimal_width = 50;
+    textfield.style.maximal_width = 50;
+    --[end] Main frame -> height setting
+
+    --[beg] Main frame -> zoom setting
+    local zoom_flow = mainFrame.add({type="flow", name="zoom_flow", direction="horizontal"});
+    zoom_flow.add({type="label", caption={"settings.playerEdit.zoom"}});
+    local textfield = zoom_flow.add({type="textfield", name="textfield", style="number_textfield_style"});
+    textfield.text=global.players[playerEditName].zoom;
+    textfield.style.minimal_width = 50;
+    textfield.style.maximal_width = 50;
+    --[end] Main frame -> zoom setting
+
+    --[beg] Main frame -> showGui setting
+    local showGui_flow = mainFrame.add({type="flow", name="showGui_flow", direction="horizontal"});
+    showGui_flow.add({type="checkbox", name="checkbox", caption={"settings.playerEdit.showGui"}, state=global.players[playerEditName].showGui});
+    --[end] Main frame -> showGui setting
+
+    --[beg] Main frame -> showAltInfo setting
+    local showAltInfo_flow = mainFrame.add({type="flow", name="showAltInfo_flow", direction="horizontal"});
+    showAltInfo_flow.add({type="checkbox", name="checkbox", caption={"settings.playerEdit.showAltInfo"}, state=global.players[playerEditName].showAltInfo});
+    --[end] Main frame -> showAltInfo setting
+
+    --Main frame buttons
+    local buttons = rootFrame.add({type="flow", name="buttons", direction="horizontal"});
+    buttons.style.minimal_width = 500;
+    buttons.style.maximal_width = 1000;
+    buttons.add({type="button", name="CTLM_settings_playerEdit_close", caption={"settings.cancel"}});
+    buttons.add({type="button", name="CTLM_settings_playerEdit_save", caption={"settings.save"}});
+
+end
+
+function CTLM.gui.CTLM_settings_playerEdit_close(event)
+    local player = game.get_player(event.player_index);
+    if player.gui.center.CTLM_settings_playerEdit ~= nil then
+        player.gui.center.CTLM_settings_playerEdit.destroy();
+    end
+end
+
+function CTLM.gui.CTLM_settings_playerEdit_save(event)
+    local player = game.get_player(event.player_index);
+    local PlayerEditFrame = player.gui.center.CTLM_settings_playerEdit.main;
+    local playerEditName = PlayerEditFrame.caption;
+    local enabled = PlayerEditFrame.enabled_flow.checkbox.state;
+    local dayOnly = PlayerEditFrame.dayOnly_flow.checkbox.state;
+    local width = tonumber(PlayerEditFrame.width_flow.textfield.text);
+    local height = tonumber(PlayerEditFrame.height_flow.textfield.text);
+    local zoom = tonumber(PlayerEditFrame.zoom_flow.textfield.text);
+    local showGui = PlayerEditFrame.showGui_flow.checkbox.state;
+    local showAltInfo = PlayerEditFrame.showAltInfo_flow.checkbox.state;
+
+    CTLM.log("playerEditName:" .. playerEditName);
+    CTLM.log("enabled:" .. tostring(enabled));
+    CTLM.log("dayOnly:" .. tostring(dayOnly));
+    CTLM.log("width:" .. width);
+    CTLM.log("height:" .. height);
+    CTLM.log("zoom:" .. zoom);
+    CTLM.log("showGui:" .. tostring(showGui));
+    CTLM.log("showAltInfo:" .. tostring(showAltInfo));
+
+    global.players[playerEditName].enabled = enabled;
+    global.players[playerEditName].dayOnly = dayOnly;
+    global.players[playerEditName].width = width;
+    global.players[playerEditName].height = height;
+    global.players[playerEditName].zoom = zoom;
+    global.players[playerEditName].showGui = showGui;
+    global.players[playerEditName].showAltInfo = showAltInfo;
 end
